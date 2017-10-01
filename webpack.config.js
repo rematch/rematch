f52db3856e03e082b7f8ea4ddb0ae44c611f7eb1
shmck <shawn.j.mckay@gmail.com>
2017-09-30 20:04:27 -0700

    add jest

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,47 @@
-/* global __dirname, require, module*/
+/* global __dirname, require, module */
 
 const webpack = require('webpack')
-const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin
 const path = require('path')
 const env = require('yargs').argv.env // use --env with webpack 2
 
+const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin
+
 const libraryName = 'rematch'
 
-let plugins = [],
-	outputFile
+const plugins = []
+let outputFile
 
 if (env === 'build') {
-	plugins.push(new UglifyJsPlugin({ minimize: true }))
-	outputFile = libraryName + '.min.js'
+  plugins.push(new UglifyJsPlugin({ minimize: true }))
+  outputFile = `${libraryName}.min.js`
 } else {
-	outputFile = libraryName + '.js'
+  outputFile = `${libraryName}.js`
 }
 
 const config = {
-	entry: __dirname + '/src/index.js',
-	devtool: 'source-map',
-	output: {
-		path: __dirname + '/lib',
-		filename: outputFile,
-		library: libraryName,
-		libraryTarget: 'umd',
-		umdNamedDefine: true,
-	},
-	module: {
-		rules: [
-			{
-				test: /(\.jsx|\.js)$/,
-				loader: 'babel-loader',
-				exclude: /(node_modules|bower_components)/,
-			},
-			{
-				test: /(\.jsx|\.js)$/,
-				loader: 'eslint-loader',
-				exclude: /node_modules/,
-			},
-		],
-	},
-	resolve: {
-		modules: [path.resolve('./node_modules'), path.resolve('./src')],
-		extensions: ['.json', '.js'],
-	},
-	plugins: plugins,
+  entry: `${__dirname}/src/index.js`,
+  devtool: 'source-map',
+  output: {
+    path: `${__dirname}/lib`,
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel-loader!eslint-loader',
+        exclude: /(node_modules|bower_components)/,
+      },
+    ],
+  },
+  resolve: {
+    modules: [path.resolve('./node_modules'), path.resolve('./src')],
+    extensions: ['.json', '.js'],
+  },
+  plugins,
 }
 
 module.exports = config
