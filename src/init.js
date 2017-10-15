a108c39622eb605c5fbd8516012310d2bcc47658
shmck <shawn.j.mckay@gmail.com>
2017-10-14 21:19:31 -0700

    closes #3. closes #30. use dispatch directly as store.dispatch

--- src/init.js
+++ src/init.js
@@ -1,6 +1,9 @@
 // @flow
 import { validateConfig } from './validate'
 import { createStore } from './store'
+import { createDispatch } from './dispatch'
+
+export const localConfig = {}
 
 /**
  * init
@@ -8,4 +11,5 @@ import { createStore } from './store'
 export default (config: $config = {}): void => {
   validateConfig(config)
   createStore(config.initialState, config.middleware, config.extraReducers)
+  createDispatch()
 }
