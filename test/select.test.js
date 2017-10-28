9779ec6795813e5d42c5dc0ed28cf0b1daa75bad
Blair <blairbodnar@gmail.com>
2017-10-28 11:39:56 -0700

    Removed autobind for selectors. Added tests with `reselect`

--- test/select.test.js
+++ test/select.test.js
@@ -20,7 +20,7 @@ describe('select:', () => {
   })
 
   it('should allow access to the selector', () => {
-    const { init, model, select } = require('../src')
+    const { init, model, select, getStore } = require('../src')
     init()
     model({
       name: 'a',
@@ -32,12 +32,13 @@ describe('select:', () => {
         double: s => s * 2
       },
     })
-    const doubled = select.a.double()
+    const state = getStore().getState()
+    const doubled = select.a.double(state)
     expect(doubled).toBe(4)
   })
 
   it('should allow passing in of params toselector', () => {
-    const { init, model, select } = require('../src')
+    const { init, model, select, getStore } = require('../src')
     init()
     model({
       name: 'a',
@@ -49,8 +50,100 @@ describe('select:', () => {
         prependWithLetter: (s, letter) => letter + s
       },
     })
-    const prepended = select.a.prependWithLetter('P')
+    const state = getStore().getState()
+    const prepended = select.a.prependWithLetter(state, 'P')
     expect(prepended).toBe('P2')
+  })
+
+  describe('reselect: ', () => {
+
+    it('should allow for createSelector to be used instead of a normal selector', () => {
+      const { init, model, select, getStore } = require('../src')
+      const { createSelector } = require('reselect')
+      init()
+      model({
+        name: 'count',
+        state: 2,
+        selectors: {
+          double: createSelector(
+            state => state,
+            count => count * 2
+          )
+        },
+      })
+      const state = getStore().getState()
+      const doubled = select.count.double(state)
+      expect(doubled).toBe(4)
+    })
+
+    it('should allow createSelector to be used outside of a model', () => {
+      const { init, model, select, getStore } = require('../src')
+      const { createSelector } = require('reselect')
+      init()
+      model({
+        name: 'countA',
+        state: 2,
+        selectors: {
+          double: state => state * 2
+        }
+      })
+      model({
+        name: 'countB',
+        state: 10,
+        selectors: {
+          double: state => state * 2
+        }
+      })
+
+      const outsideSelector = createSelector(
+        select.countA.double,
+        select.countB.double,
+        (countADoubled, countBDoubled) => countADoubled + countBDoubled
+      )
+
+      const state = getStore().getState()
+      const result = outsideSelector(state)
+      expect(result).toBe(24)
+    })
+
+    it('should allow for mixing normal selectors and reselect selectors', () => {
+      const { init, model, select, getStore } = require('../src')
+      const { createSelector } = require('reselect')
+      init()
+      model({
+        name: 'countA',
+        state: 2,
+        selectors: {
+          double: state => state * 2,
+          plusOne: createSelector(
+            state => state,
+            countA => countA + 1
+          )
+        }
+      })
+      model({
+        name: 'countB',
+        state: 10,
+        selectors: {
+          double: createSelector(
+            state => state,
+            countB => countB * 2
+          )
+        }
+      })
+
+      const outsideSelector = createSelector(
+        select.countA.double,
+        select.countA.plusOne,
+        select.countB.double,
+        (countADoubled, countAPlusOne, countBDoubled) =>
+          countADoubled + countAPlusOne + countBDoubled
+      )
+
+      const state = getStore().getState()
+      const result = outsideSelector(state)
+      expect(result).toBe(27)
+    })
   })
 })
 
