63356b76aadbb2006322b1ad1a4739ae13eeb280
shmck <shawn.j.mckay@gmail.com>
2017-11-04 13:44:32 -0700

    update readme, add config

--- plugins/loading/src/index.js
+++ plugins/loading/src/index.js
@@ -12,40 +12,43 @@ const createLoadingAction = (show) => (state, { name, action }) => ({
   }
 })
 
-export default () => ({
-  init: ({ dispatch }) => ({
-    model: {
-      name: 'loading',
-      state: {
-        global: false,
-        models: {},
-        effects: {}
-      },
-      reducers: {
-        show: createLoadingAction(true),
-        hide: createLoadingAction(false),
-      }
-    },
-    onModel({ name }) {
-      // do not run dispatch on loading model
-      if (name === 'loading') { return }
-      const modelActions = dispatch[name]
-      // map over effects within models
-      Object.keys(modelActions).forEach(action => {
-        if (dispatch[name][action].isEffect) {
-          // copy function
-          const fn = dispatch[name][action]
-          // create function with pre & post loading calls
-          const dispatchWithHooks = async function dispatchWithHooks(props) {
-            dispatch.loading.show({ name, action })
-            await fn(props)
-            // waits for dispatch function to finish before calling "hide"
-            dispatch.loading.hide({ name, action })
-          }
-          // replace existing effect with new dispatch
-          dispatch[name][action] = dispatchWithHooks
+export default (config = {}) => {
+  const loadingModelName = config.name || 'loading'
+  return {
+    init: ({ dispatch }) => ({
+      model: {
+        name: loadingModelName,
+        state: {
+          global: false,
+          models: {},
+          effects: {}
+        },
+        reducers: {
+          show: createLoadingAction(true),
+          hide: createLoadingAction(false),
         }
-      })
-    }
-  })
-})
+      },
+      onModel({ name }) {
+      // do not run dispatch on loading model
+        if (name === loadingModelName) { return }
+        const modelActions = dispatch[name]
+        // map over effects within models
+        Object.keys(modelActions).forEach(action => {
+          if (dispatch[name][action].isEffect) {
+          // copy function
+            const fn = dispatch[name][action]
+            // create function with pre & post loading calls
+            const dispatchWithHooks = async function dispatchWithHooks(props) {
+              dispatch.loading.show({ name, action })
+              await fn(props)
+              // waits for dispatch function to finish before calling "hide"
+              dispatch.loading.hide({ name, action })
+            }
+            // replace existing effect with new dispatch
+            dispatch[name][action] = dispatchWithHooks
+          }
+        })
+      }
+    })
+  }
+}
