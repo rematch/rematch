f154fe9050d131c515f303591f896ee728fdb697
shmck <shawn.j.mckay@gmail.com>
2017-11-06 21:06:20 -0800

    add failing persist tests that outline issue

--- plugins/persist/test/persist.test.js
+++ plugins/persist/test/persist.test.js
@@ -61,35 +61,42 @@ describe('persist', () => {
   test('should work with init models', () => {
     const persistPlugin = require('../src').default
     const { getPersistor } = require('../src')
-    const { init } = require('../../../src')
+    const { init, dispatch, getStore } = require('../../../src')
     const a = {
       name: 'a',
-      state: { b: 1 }
+      state: { b: 1 },
+      reducers: {
+        addOne: s => ({ b: s.b + 1 })
+      }
     }
     init({
-      initialState: {},
       plugins: [persistPlugin()],
       models: { a }
     })
+    dispatch.a.addOne()
     const persistor = getPersistor()
     expect(persistor.purge).toBeDefined()
+    expect(getStore().getState()).toBe({ b: 2 })
   })
 
-  // persist requires models to be run on init
-  // test('should load with model() instead of extra reducers', () => {
-  //   const { init, model, getStore } = require('../../../src')
-  //   const persistPlugin = require('../src').default
-  //   init({
-  //     initialState: {},
-  //     plugins: [persistPlugin()],
-  //   })
-  //   model({
-  //     name: 'a',
-  //     state: 0,
-  //     reducers: {
-  //       addOne: s => s + 1,
-  //     }
-  //   })
-  //   expect(getStore().getState()._persist).toEqual(defaultPersist)
-  // })
+  // NOTE: persist may require models to be run on init. Unsure.
+  test('should load with model() instead of extra reducers', () => {
+    const {
+      init, model, dispatch, getStore
+    } = require('../../../src')
+    const persistPlugin = require('../src').default
+    init({
+      plugins: [persistPlugin()],
+    })
+    model({
+      name: 'a',
+      state: { b: 1 },
+      reducers: {
+        addOne: s => ({ b: s.b + 1 })
+      }
+    })
+    dispatch.a.addOne()
+    expect(getStore().getState()._persist).toEqual(defaultPersist)
+    expect(getStore().getState()).toBe({ b: 2 })
+  })
 })
