2930089131ace4d648f6bcb2035e05ca1eaa769b
shmck <shawn.j.mckay@gmail.com>
2017-11-07 12:00:50 -0800

    remove babel-runtime in favor of babel-polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ if (env === 'build') {
 }
 
 const config = {
-  entry: `${__dirname}/src/index.js`,
+  entry: ['babel-polyfill', `${__dirname}/src/index.js`],
   devtool: 'source-map',
   output: {
     path: `${__dirname}/lib`,
