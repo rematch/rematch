b83aaad1666e9aad3af26dd66e6430fb64519134
shmck <shawn.j.mckay@gmail.com>
2017-11-12 22:47:55 -0800

    use "transform-runtime" over "babel-polyfill"

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ if (env === 'build') {
 }
 
 const config = {
-  entry: ['babel-polyfill', `${__dirname}/src/index.js`],
+  entry: `${__dirname}/src/index.js`,
   devtool: 'source-map',
   output: {
     path: `${__dirname}/lib`,
