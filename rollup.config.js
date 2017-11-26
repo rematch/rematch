a5e3e7096667e981871010ceeede58959aca0e14
shmck <shawn.j.mckay@gmail.com>
2017-11-26 15:49:09 -0800

    fix es-modules build

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,10 @@ import replace from 'rollup-plugin-replace'
 import uglify from 'rollup-plugin-uglify'
 import commonJs from 'rollup-plugin-commonjs'
 
+import { minify } from 'uglify-es'
+// experimental minifier to es modules
+// https://github.com/TrySound/rollup-plugin-uglify#warning
+
 const pkg = require('./package.json')
 
 const env = process.env.NODE_ENV
@@ -9,12 +13,13 @@ const env = process.env.NODE_ENV
 const config = {
   input: 'lib/index.js',
   sourcemap: true,
-  external: [],
   plugins: [
     replace({
       'process.env.NODE_ENV': JSON.stringify(env),
     }),
-    commonJs()
+    commonJs({
+      // include: ['node_modules/redux/**']
+    })
   ],
 }
 
@@ -29,14 +34,38 @@ if (env === 'production') {
   }))
 }
 
-export default [Object.assign({}, config, {
+export default [
+  // Universal Module
+  Object.assign({}, config, {
   name: 'rematch',
   output: [
     { file: pkg.browser, format: 'umd', exports: 'named' },
   ],
-}), Object.assign({}, config, {
+}),
+  // CommonJS
+  Object.assign({}, config, {
   output: [
     { file: pkg.main, format: 'cjs', exports: 'named' },
-    // { file: pkg.module, format: 'es', exports: 'named' },
   ],
-})]
+}),
+  // ES Modules
+  Object.assign({}, config, {
+    output: {
+      file: pkg.module, format: 'es', exports: 'named',
+    },
+    plugins: [
+      replace({
+        'process.env.NODE_ENV': JSON.stringify(env),
+      }),
+      commonJs(),
+      uglify({
+        compress: {
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true,
+          warnings: false,
+        },
+      }, minify)
+    ],
+  })
+]
