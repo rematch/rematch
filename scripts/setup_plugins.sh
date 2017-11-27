6c0693a9f0ca4993fe5c6fb2a70a533f35f9ff3a
shmck <shawn.j.mckay@gmail.com>
2017-11-26 22:39:53 -0800

    fix CI build (run tsc in each directory before tests)

--- scripts/setup_plugins.sh
+++ scripts/setup_plugins.sh
@@ -6,9 +6,17 @@ install_plugin_deps () {
   npm --prefix ./$1 install ./$1
 }
 
+run_typescript_compiler() {
+  cd $1
+  tsc
+  cd ../..
+}
+
 echo 'installing plugin dependencies'
 
 for plugin in 'plugins'/*
 do
+ echo 'PLUGIN: ' $plugin
  install_plugin_deps $plugin
+ run_typescript_compiler $plugin
 done
