2ee998278276dc9dea42883303fa49c24c7f9f86
shmck <shawn.j.mckay@gmail.com>
2018-01-14 17:49:36 -0800

    add basic production tests

---
+++ test/production.test.js
@@ -0,0 +1,23 @@
+beforeEach(() => {
+  jest.resetModules()
+})
+
+describe('production', () => {
+  test('should create a valid common js dev build', () => {
+    const { init } = require('../dist/cjs/rematch.dev')
+    expect(init).toBeDefined()
+  })
+  test('should create a valid common js prod build', () => {
+    const { init } = require('../dist/cjs/rematch.prod.min')
+    expect(init).toBeDefined()
+  })
+  xtest('should create a valid es module prod build', () => {
+    // fails due to export not found
+    const { init } = require('../dist/esm/rematch.prod.min')
+    expect(init).toBeDefined()
+  })
+  test('should create a valid universal module prod build', () => {
+    const rematch = require('../dist/umd/rematch.prod.min').default
+    expect(rematch.init).toBeDefined()
+  })
+})
