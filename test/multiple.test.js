b7837cf99f7ad5f61824aded4dd58cea8a2c4054
shmck <shawn.j.mckay@gmail.com>
2018-03-04 20:35:56 -0800

    add additional tests for handling multiple stores

---
+++ test/multiple.test.js
@@ -0,0 +1,59 @@
+describe('multiple stores', () => {
+  test('should not throw if multiple stores', () => {
+    const { init } = require('../src')
+    const store1 = init({})
+    expect(() => init({})).not.toThrow()
+  })
+
+  test('should store state for multiple stores', () => {
+    const { init } = require('../src')
+
+    const store1 = init({ models: { count1: { state: 0 } } })
+    const store2 = init({ models: { count2: { state: 42 } } })
+
+    expect(store1.getState()).toEqual({ count1: 0 })
+    expect(store2.getState()).toEqual({ count2: 42 })
+  })
+
+  test('should be able to store.dispatch to specific stores', () => {
+    const { init } = require('../src')
+
+    const count = {
+      state: 0,
+      reducers: {
+        increment: (state) => state + 1,
+      }
+    }
+
+    const store1 = init({ models: { count } })
+    const store2 = init({ models: { count } })
+
+    store1.dispatch.count.increment()
+
+    store2.dispatch.count.increment()
+    store2.dispatch.count.increment()
+
+    expect(store1.getState()).toEqual({ count: 1 })
+    expect(store2.getState()).toEqual({ count: 2 })
+  })
+
+  test('global dispatch should dispatch to both stores', () => {
+    const { init, dispatch } = require('../src')
+
+    const count = {
+      state: 0,
+      reducers: {
+        increment: (state) => state + 1,
+      }
+    }
+
+    const store1 = init({ models: { count } })
+    const store2 = init({ models: { count } })
+
+    dispatch.count.increment()
+    dispatch.count.increment()
+
+    expect(store1.getState()).toEqual({ count: 2 })
+    expect(store2.getState()).toEqual({ count: 2 })
+  })
+})
