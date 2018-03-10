0ac85a1a4fe2cf558017d0726bec8a002f5aebf2
Kameron Tanseli <kamerontanseliwork@gmail.com>
2018-03-10 22:15:06 +0000

    Added test file for utils folder
    Added test for deprecate.ts pushing the coverage to 100%

---
+++ test/utils.test.js
@@ -0,0 +1,34 @@
+beforeEach(() => {
+  jest.resetModules()
+})
+
+describe('deprecate', ()=> {
+  const {stub} = require('sinon')
+  const module = require('../src/utils/deprecate')
+  const deprecate = module.default
+
+  it('should call console.warn when process.env.NODE_ENV is not production', ()=> {
+      const warn = stub(console, 'warn')
+      const cache = process.env.NODE_ENV
+      process.env.NODE_ENV = 'development'
+
+      deprecate('warning')
+
+      expect(warn.called).toBe(true)
+
+      warn.restore()
+      process.env.NODE_ENV = cache
+  })
+  it('should call console.warn when process.env.NODE_ENV is not production', ()=> {
+      const warn = stub(console, 'warn')
+      const cache = process.env.NODE_ENV
+      process.env.NODE_ENV = 'production'
+
+      deprecate('warning')
+
+      expect(warn.called).toBe(false)
+
+      warn.restore()
+      process.env.NODE_ENV = cache
+  })
+})
