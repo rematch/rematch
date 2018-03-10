9b908ad6de9f4f26fcbbced6168ba540032afa0a
Kameron Tanseli <kamerontanseliwork@gmail.com>
2018-03-10 23:15:12 +0000

    Used jest.spyOn instead of sinon to spy on console.warn

--- test/utils.test.js
+++ test/utils.test.js
@@ -3,32 +3,31 @@ beforeEach(() => {
 })
 
 describe('deprecate', ()=> {
-  const {stub} = require('sinon')
   const module = require('../src/utils/deprecate')
   const deprecate = module.default
 
   it('should call console.warn when process.env.NODE_ENV is not production', ()=> {
-      const warn = stub(console, 'warn')
+      const warn = jest.spyOn(console, 'warn')
       const cache = process.env.NODE_ENV
       process.env.NODE_ENV = 'development'
 
       deprecate('warning')
+      expect(warn).toHaveBeenCalled()
 
-      expect(warn.called).toBe(true)
-
-      warn.restore()
+      warn.mockReset();
+      warn.mockRestore();
       process.env.NODE_ENV = cache
   })
   it('should not call console.warn when process.env.NODE_ENV is production', ()=> {
-      const warn = stub(console, 'warn')
-      const cache = process.env.NODE_ENV
-      process.env.NODE_ENV = 'production'
+    const warn = jest.spyOn(console, 'warn')
+    const cache = process.env.NODE_ENV
+    process.env.NODE_ENV = 'production'
 
-      deprecate('warning')
+    deprecate('warning')
+    expect(warn).not.toHaveBeenCalled()
 
-      expect(warn.called).toBe(false)
-
-      warn.restore()
-      process.env.NODE_ENV = cache
+    warn.mockReset();
+    warn.mockRestore();
+    process.env.NODE_ENV = cache
   })
 })
