a95e08f0bba9c726ab177d7f3b522b4d79464f08
Blair Bodnar <blairbodnar@gmail.com>
2018-04-04 00:09:31 -0700

    Model reducers may subscribe to non-model actions via `#` prefix

--- test/listener.test.js
+++ test/listener.test.js
@@ -1,3 +1,7 @@
+beforeEach(() => {
+  jest.resetModules()
+})
+
 describe('listener', () => {
   test('should trigger state changes on another models reducers', () => {
     const { init, dispatch } = require('../src')
@@ -23,5 +27,73 @@ describe('listener', () => {
     dispatch.count1.increment(1)
 
     expect(store.getState()).toEqual({ count1: 1, count2: 1 })
+  })
+
+  test('should trigger state changes on non-model reducers', () => {
+    const { init, dispatch } = require('../src')
+
+    const count = {
+      state: 0,
+      reducers: {
+        '#NON_MODEL_ACTION': (state, payload) => state + payload,
+      }
+    }
+
+    const store = init({
+      models: { count }
+    })
+
+    dispatch({ type: '#NON_MODEL_ACTION', payload: 1 })
+
+    expect(store.getState()).toEqual({ count: 1 })
+  })
+
+  test('should trigger state changes for all three types of listeners on a model', () => {
+    const { init, dispatch } = require('../src')
+
+    const count1 = {
+      state: 0,
+      reducers: {
+        increment: (state, payload) => state + payload,
+        'count2/increment': (state, payload) => state + payload,
+        '#increment': (state, payload) => state + payload,
+      }
+    }
+
+    const count2 = {
+      state: 0,
+      reducers: {
+        increment: (state, payload) => state + payload,
+      }
+    }
+
+    const store = init({
+      models: { count1, count2 }
+    })
+
+    dispatch.count1.increment(1)
+    dispatch.count2.increment(1)
+    dispatch({ type: '#increment', payload: 1 })
+
+    expect(store.getState()).toEqual({ count1: 3, count2: 1 })
+  })
+
+  test('should correctly handle non-model action type with a "/" in it', () => {
+    const { init, dispatch } = require('../src')
+
+    const count = {
+      state: 0,
+      reducers: {
+        '#something/else': (state, payload) => state + payload,
+      }
+    }
+
+    const store = init({
+      models: { count }
+    })
+
+    dispatch({ type: '#something/else', payload: 1 })
+
+    expect(store.getState()).toEqual({ count: 1 })
   })
 })
