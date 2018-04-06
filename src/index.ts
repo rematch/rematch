6b132fbe8d0e7b3b683cddd24d8013df79a27747
shmck <shawn.j.mckay@gmail.com>
2018-04-06 08:27:58 -0700

    add fixes for case of reducer/effect with same names

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import mergeConfig from './utils/mergeConfig'
 
 // allows for global dispatch to multiple stores
 const stores = {}
-const effects = {}
+const dispatches = {}
 
 /**
  * global Dispatch
@@ -52,20 +52,16 @@ export const init = (initConfig: R.InitConfig = {}): R.RematchStore => {
     for (const actionName of Object.keys(store.dispatch[modelName])) {
       if (!isListener(actionName)) {
         const action = store.dispatch[modelName][actionName]
-        if (!action.isEffect) {
-          dispatch[modelName][actionName] = action
-        } else {
-          if (!effects[modelName]) {
-            effects[modelName] = {}
-          }
-          if (!effects[modelName][actionName]) {
-            effects[modelName][actionName] = {}
-          }
-          effects[modelName][actionName][name] = action
-          dispatch[modelName][actionName] = (payload: any, meta: any) => {
-            for (const storeName of Object.keys(effects[modelName][actionName])) {
-              stores[storeName].dispatch[modelName][actionName](payload, meta)
-            }
+        if (!dispatches[modelName]) {
+          dispatches[modelName] = {}
+        }
+        if (!dispatches[modelName][actionName]) {
+          dispatches[modelName][actionName] = {}
+        }
+        dispatches[modelName][actionName][name] = action
+        dispatch[modelName][actionName] = (payload: any, meta: any) => {
+          for (const storeName of Object.keys(dispatches[modelName][actionName])) {
+            stores[storeName].dispatch[modelName][actionName](payload, meta)
           }
         }
       }
