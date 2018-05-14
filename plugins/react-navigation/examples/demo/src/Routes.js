8136acb0666882415443be0c4bc1510e5056fd85
Emerson Laurentino <emersonlaurentino@hotmail.com>
2018-05-14 20:01:38 -0300

    remove unused imports
    removes imports of React and Redux that were not being used

--- plugins/react-navigation/examples/demo/src/Routes.js
+++ plugins/react-navigation/examples/demo/src/Routes.js
@@ -1,7 +1,4 @@
-import React from 'react';
-import { connect } from 'react-redux';
 import { StackNavigator } from 'react-navigation';
-
 import LoginScreen from './components/LoginScreen';
 import MainScreen from './components/MainScreen';
 import ProfileScreen from './components/ProfileScreen';
