6b46bf60614b10162ff5f0e9c1bfd5d18e9bdad1
shmck <shawn.j.mckay@gmail.com>
2018-06-02 12:07:55 -0700

    ignore select ambient must be string errors

--- plugins/select/index.d.ts
+++ plugins/select/index.d.ts
@@ -1,8 +1,10 @@
 import index from './dist';
 import { ExtractRematchSelectorsFromModels, Models } from '../../typings/rematch/index'
 
+// @ts-ignore
 export const select = {}
 
+// @ts-ignore
 export function getSelect<M extends Models = Models>() {
   return select as ExtractRematchSelectorsFromModels<M>
 }
