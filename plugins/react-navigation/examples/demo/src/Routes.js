9679e3560c258d1f2e3c36d7458e0826365d1184
Eric Kim <bosung90@gmail.com>
2018-06-12 15:42:07 -0700

    Upgrade react-navigation-plugin to react-navigation v2

--- plugins/react-navigation/examples/demo/src/Routes.js
+++ plugins/react-navigation/examples/demo/src/Routes.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { StackNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation'
 
-import LoginScreen from './components/LoginScreen';
-import MainScreen from './components/MainScreen';
-import ProfileScreen from './components/ProfileScreen';
+import LoginScreen from './components/LoginScreen'
+import MainScreen from './components/MainScreen'
+import ProfileScreen from './components/ProfileScreen'
 
-export default StackNavigator({
-  Login: { screen: LoginScreen },
-  Main: { screen: MainScreen },
-  Profile: { screen: ProfileScreen },
-});
-
+export default createStackNavigator({
+	Login: { screen: LoginScreen },
+	Main: { screen: MainScreen },
+	Profile: { screen: ProfileScreen },
+})
