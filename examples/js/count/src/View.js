7856ae2b8419085f628247a7dd3a6887a7d9eb65
shmck <shawn.j.mckay@gmail.com>
2018-06-22 19:51:09 -0700

    remove global dispatch from examples

--- examples/js/count/src/View.js
+++ examples/js/count/src/View.js
@@ -1,16 +1,14 @@
-import { dispatch } from '@rematch/core'
+export default store => {
+	// select html elements
+	const countEl = document.getElementById('count')
+	const incrementEl = document.getElementById('increment')
 
-export default (store) => {
-  // select html elements
-  const countEl = document.getElementById('count')
-  const incrementEl = document.getElementById('increment')
+	// add onClick listener
+	incrementEl.addEventListener('click', () => store.dispatch.count.addOne())
 
-  // add onClick listener
-  incrementEl.addEventListener('click', () => dispatch.count.addOne())
-
-  // setup store store subscription
-  store.subscribe(() => {
-    const state = store.getState()
-    countEl.value = state.count
-  })
+	// setup store store subscription
+	store.subscribe(() => {
+		const state = store.getState()
+		countEl.value = state.count
+	})
 }
