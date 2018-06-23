8a31ad35b1e688dde48bf141b1a7ed3640140490
shmck <shawn.j.mckay@gmail.com>
2018-06-23 11:06:31 -0700

    update examples

--- examples/react/multi-store/src/components/Counter.js
+++ examples/react/multi-store/src/components/Counter.js
@@ -1,36 +1,33 @@
 import React from 'react'
 
 const styles = {
-  container: {
-    width: 50,
-    height: 50,
-    border: '2px solid black',
-    borderRadius: 50,
-    backgroundColor: 'lightblue',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    color: 'white',
-    fontSize: 24,
-  }
+	container: {
+		width: 50,
+		height: 50,
+		border: '2px solid black',
+		borderRadius: 50,
+		backgroundColor: 'lightblue',
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	text: {
+		color: 'white',
+		fontSize: 24,
+	},
 }
 
 export default class Counter extends React.Component {
-  onClick = () => {
-    this.props.store.dispatch.count.increment()
-    this.forceUpdate()
-  }
-  render() {
-    const { count } = this.props.store.getState()
-    return (
-      <div
-        style={styles.container}
-        onClick={this.onClick}
-      >
-        <span style={styles.text}>{count}</span>
-      </div>
-    )
-  }
+	onClick = () => {
+		this.props.store.dispatch.count.increment()
+		this.forceUpdate()
+	}
+	render() {
+		const { count } = this.props.store.getState()
+		return (
+			<div style={styles.container} onClick={this.onClick}>
+				<span style={styles.text}>{count}</span>
+			</div>
+		)
+	}
 }
