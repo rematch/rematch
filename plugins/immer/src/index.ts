5c4ff6b7efe046cd434c50cceaf227761195fb50
linonetwo <linonetwo012@gmail.com>
2018-07-28 18:12:13 +0800

    add whitelist and blacklist to immer plugin

--- plugins/immer/src/index.ts
+++ plugins/immer/src/index.ts
@@ -2,32 +2,62 @@ import { Models, Plugin } from '@rematch/core'
 import produce from 'immer'
 import { combineReducers, ReducersMapObject } from 'redux'
 
-function combineReducersWithImmer(reducers: ReducersMapObject) {
-	const reducersWithImmer = {}
+export interface ImmerConfig {
+	whitelist?: string[];
+	blacklist?: string[];
+}
 
-	// reducer must return value because literal don't support immer
-	for (const [key, reducerFn] of Object.entries(reducers)) {
-		reducersWithImmer[key] = (state, payload) => {
-			if (typeof state === 'object') {
-				return produce(state, (draft: Models) => {
-					reducerFn(draft, payload)
-				})
-			} else {
-				return reducerFn(state, payload)
+const validateConfig = config => {
+	if (config.whitelist && !Array.isArray(config.whitelist)) {
+		throw new Error('immer plugin config whitelist must be an array of strings')
+	}
+	if (config.blacklist && !Array.isArray(config.blacklist)) {
+		throw new Error('immer plugin config blacklist must be an array of strings')
+	}
+	if (config.whitelist && config.blacklist) {
+		throw new Error(
+			'immer plugin config cannot have both a whitelist & a blacklist'
+		)
+	}
+}
+
+function getCombineReducersWithImmer(config: ImmerConfig) {
+	return (reducers: ReducersMapObject) => {
+		const reducersWithImmer = {}
+
+		// reducer must return value because literal don't support immer
+		for (const [key, reducerFn] of Object.entries(reducers)) {
+			// ignore reducers not in whitelist
+			if (config.whitelist && !config.whitelist.includes(key)) {
+				continue
+			}
+			// ignore reducers in blacklist
+			if (config.blacklist && config.blacklist.includes(key)) {
+				continue
+			}
+			reducersWithImmer[key] = (state, payload) => {
+				if (typeof state === 'object') {
+					return produce(state, (draft: Models) => {
+						reducerFn(draft, payload)
+					})
+				} else {
+					return reducerFn(state, payload)
+				}
 			}
 		}
-	}
 
-	return combineReducers(reducersWithImmer)
+		return combineReducers(reducersWithImmer)
+	}
 }
 
 // rematch plugin
-const immerPlugin = (): Plugin => ({
-	config: {
-		redux: {
-			combineReducers: combineReducersWithImmer,
+export default (config: ImmerConfig = {}): Plugin => {
+	validateConfig(config)
+	return {
+		config: {
+			redux: {
+				combineReducers: getCombineReducersWithImmer(config),
+			},
 		},
-	},
-})
-
-export default immerPlugin
+	}
+}
