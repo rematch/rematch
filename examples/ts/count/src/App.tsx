5e105b3212775344d3057cfedefd62a71bfdc5b7
shmck <shawn.j.mckay@gmail.com>
2018-09-30 10:49:12 -0700

    clean up example typings

--- examples/ts/count/src/App.tsx
+++ examples/ts/count/src/App.tsx
@@ -1,15 +1,14 @@
 import * as React from 'react'
 import { connect } from 'react-redux'
-import { RematchDispatch, RematchRootState } from '@rematch/core'
 
-import { models } from './store'
+import { iRootState, Dispatch } from './store'
 
-const mapState = (state: RematchRootState<models>) => ({
+const mapState = (state: iRootState) => ({
 	dolphins: state.dolphins,
 	sharks: state.sharks,
 })
 
-const mapDispatch = (dispatch: RematchDispatch<models>) => ({
+const mapDispatch = (dispatch: Dispatch) => ({
 	incrementDolphins: dispatch.dolphins.increment,
 	incrementDolphinsAsync: dispatch.dolphins.incrementAsync,
 	incrementSharks: () => dispatch.sharks.increment(1),
@@ -17,26 +16,30 @@ const mapDispatch = (dispatch: RematchDispatch<models>) => ({
 	incrementSharksAsync2: () => dispatch({ type: 'sharks/incrementAsync', payload: 2 }),
 })
 
-interface CountProps extends Partial<ReturnType<typeof mapState>>,
-Partial<ReturnType<typeof mapDispatch>> {}
+type connectedProps = ReturnType<typeof mapState> & ReturnType<typeof mapDispatch>
+type Props = connectedProps
 
-const Count: React.SFC<CountProps> = props => (
-	<div style={{ display: 'flex', flexDirection: 'row' }}>
-		<div style={{ width: 120 }}>
-			<h3>Dolphins</h3>
-			<h1>{props.dolphins}</h1>
-			<button onClick={props.incrementDolphins}>+1</button>
-			<button onClick={props.incrementDolphinsAsync}>Async +1</button>
-		</div>
-		<div style={{ width: 200 }}>
-			<h3>Sharks</h3>
-			<h1>{props.sharks}</h1>
-			<button onClick={props.incrementSharks}>+1</button>
-			<button onClick={props.incrementSharksAsync}>Async +1</button>
-			<button onClick={props.incrementSharksAsync2}>Async +2</button>
-		</div>
-		<p>Using Rematch Models</p>
-	</div>
-)
+class Count extends React.Component<Props> {
+	render() {
+		return (
+			<div style={{ display: 'flex', flexDirection: 'row' }}>
+				<div style={{ width: 120 }}>
+					<h3>Dolphins</h3>
+					<h1>{this.props.dolphins}</h1>
+					<button onClick={this.props.incrementDolphins}>+1</button>
+					<button onClick={this.props.incrementDolphinsAsync}>Async +1</button>
+				</div>
+				<div style={{ width: 200 }}>
+					<h3>Sharks</h3>
+					<h1>{this.props.sharks}</h1>
+					<button onClick={this.props.incrementSharks}>+1</button>
+					<button onClick={this.props.incrementSharksAsync}>Async +1</button>
+					<button onClick={this.props.incrementSharksAsync2}>Async +2</button>
+				</div>
+				<p>Using Rematch Models</p>
+			</div>
+		)
+	}
+}
 
-export default connect(mapState as any, mapDispatch as any)(Count)
+export default connect(mapState, mapDispatch)(Count)
--- examples/ts/count/src/store.ts
+++ examples/ts/count/src/store.ts
@@ -1,10 +1,10 @@
-import { init } from '@rematch/core'
-
+import { init, RematchRootState } from '@rematch/core'
 import * as models from './models'
-
-export { models }
-export type models = typeof models
 
 export const store = init({
 	models,
 })
+
+export type Store = typeof store
+export type Dispatch = typeof store.dispatch
+export type iRootState = RematchRootState<typeof models>
