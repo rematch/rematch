5173255c107ebf14168c0a6c9a3e49393aca4edf
Sam Richard <sam.richard@gmail.com>
2018-11-11 04:28:58 -0700

    Refactor Lift action creator type to model namespace in created dispatcher

--- plugins/port/src/port.ts
+++ plugins/port/src/port.ts
@@ -1,74 +1,119 @@
-import { ModelConfig, Plugin, RematchStore } from '@rematch/core'
-import {
-	ActionCreatorsMapObject,
-	combineReducers,
-	Middleware,
-	Reducer,
-	ReducersMapObject,
-} from 'redux'
+import * as Rematch from '@rematch/core'
+import * as Redux from 'redux'
 
 interface PortConfig {
 	modelName?: string
 	initialState?: any
-	actionCreators?: ActionCreatorsMapObject
-	reducer?: Reducer
-	middleware?: Middleware
-	expose?: {} | ((store: RematchStore) => {})
-	combineReducers?: typeof combineReducers
-	mapReducers?: (reducers: ReducersMapObject) => ReducersMapObject
-	mapRootReducer?: (rootReducer: Reducer) => Reducer
+	actionCreators?: Redux.ActionCreatorsMapObject
+	reducer?: Redux.Reducer
+	middleware?: Redux.Middleware
+	expose?: {} | ((store: Rematch.RematchStore) => {})
+	combineReducers?: typeof Redux.combineReducers
+	mapReducers?: (reducers: Redux.ReducersMapObject) => Redux.ReducersMapObject
+	mapRootReducer?: (rootReducer: Redux.Reducer) => Redux.Reducer
 }
 
-export default function port(config: PortConfig = {}): Plugin {
-	const redux = { combineReducers: config.combineReducers }
+export default function port(config: PortConfig = {}): Rematch.Plugin {
+	validate(config)
 
-	const model: ModelConfig = config.modelName
-		? {
-				name: config.modelName,
-				baseReducer: config.reducer,
-				state: config.initialState || null,
-		  }
-		: null
+	const redux = createReduxConfig(config)
+	const model = createModelConfig(config)
 
 	return {
 		config: {
 			redux,
-			models: model ? { [config.modelName]: model } : {},
+			models: model ? { [model.name]: model } : {},
 		},
 		middleware: config.middleware,
 		onInit() {
-			if (config.mapReducers) {
-				const combine = this.config.redux.combineReducers || combineReducers
-
-				this.config.redux.combineReducers = reducers =>
-					combine(config.mapReducers(reducers))
-			}
-
-			if (config.mapRootReducer) {
-				const combine = this.config.redux.combineReducers || combineReducers
-
-				this.config.redux.combineReducers = reducers =>
-					config.mapRootReducer(combine(reducers))
-			}
+			this.config.redux = enhanceReduxConfig(this.config.redux, config)
 		},
 		onStoreCreated(store) {
-			if (config.actionCreators) {
-				this.validate([
-					[
-						!model,
-						'ported action creators require a model name to namespace them',
-					],
-				])
-
-				for (const actionName of Object.keys(config.actionCreators)) {
-					this.dispatch[config.modelName][actionName] = (...args) =>
-						this.dispatch(config.actionCreators[actionName](...args))
-				}
+			if (model) {
+				Object.assign(
+					this.dispatch[model.name],
+					createDispatchers(this.dispatch, model.name, config)
+				)
 			}
 
 			return typeof config.expose === 'function'
 				? config.expose(store)
 				: config.expose
 		},
+	}
+}
+
+function validate(config: PortConfig) {
+	const depends = ['reducer', 'initialState', 'actionCreators'].filter(
+		key => !!config[key]
+	)
+	if (!config.modelName && depends.length) {
+		throw new Error(
+			`The following options require a model name: ${depends.join(', ')}`
+		)
+	}
+}
+
+function createModelConfig(config: PortConfig): Rematch.ModelConfig {
+	if (!config.modelName) {
+		return null
+	}
+
+	return {
+		name: config.modelName,
+		baseReducer: config.reducer,
+		state: config.initialState || null,
+	}
+}
+
+function createDispatchers(
+	dispatch: Redux.Dispatch,
+	namespace: string,
+	config: PortConfig
+) {
+	const dispatchers = {}
+
+	if (config.actionCreators) {
+		for (const actionName of Object.keys(config.actionCreators)) {
+			dispatchers[actionName] = (...args) => {
+				const action = config.actionCreators[actionName](...args)
+				return Promise.all([
+					dispatch(action),
+					dispatch({ ...action, type: `${namespace}/${actionName}` }),
+				])
+			}
+		}
+	}
+
+	return dispatchers
+}
+
+// Add options to be merged
+function createReduxConfig(config: PortConfig): Rematch.InitConfigRedux {
+	return {
+		combineReducers: config.combineReducers,
+	}
+}
+
+// Enhance pre-merged options
+function enhanceReduxConfig(
+	reduxConfig: Rematch.InitConfigRedux,
+	config: PortConfig
+): Rematch.InitConfigRedux {
+	let combineReducers = reduxConfig.combineReducers || Redux.combineReducers
+
+	if (config.mapReducers) {
+		const combine = combineReducers
+		combineReducers = reducers => combine(config.mapReducers(reducers))
+	}
+
+	if (config.mapRootReducer) {
+		const combine = combineReducers
+		combineReducers = reducers => config.mapRootReducer(combine(reducers))
+	}
+
+	return {
+		...reduxConfig,
+		combineReducers,
 	}
 }
