714ac65f5d926c4df82baf3a71021e6b3cdad914
Sam Richard <sam.richard@gmail.com>
2019-07-05 15:07:48 -0600

    Remove examples from lerna. update their packages

--- packages/rollup-config/index.js
+++ packages/rollup-config/index.js
@@ -1,73 +1,77 @@
-const { resolve } = require("path")
-const { terser } = require("rollup-plugin-terser")
+const { resolve } = require('path')
+const { terser } = require('rollup-plugin-terser')
 
-const replace = require("rollup-plugin-replace")
-const typescript = require("rollup-plugin-typescript")
+const replace = require('rollup-plugin-replace')
+const typescript = require('rollup-plugin-typescript')
+const commonjs = require('rollup-plugin-commonjs')
 
-const camelize = require("./lib/camelize")
+const camelize = require('./lib/camelize')
 
 const env = process.env.NODE_ENV
-const pkg = require(resolve("package.json"))
-const pkgNamespace = camelize(pkg.name.split("/")[1])
+const pkg = require(resolve('package.json'))
+const pkgNamespace = camelize(pkg.name.split('/')[1])
 const pkgDependencies = [
-  ...Object.keys(pkg.dependencies || {}),
-  ...Object.keys(pkg.peerDependencies || {})
+	...Object.keys(pkg.dependencies || {}),
+	...Object.keys(pkg.peerDependencies || {}),
 ]
 
-const input = "src/index.ts"
+const input = 'src/index.ts'
 
 const plugins = [
-  replace({
-    "process.env.NODE_ENV": JSON.stringify(env)
-  }),
-  typescript()
+	replace({
+		'process.env.NODE_ENV': JSON.stringify(env),
+	}),
+	commonjs({
+		include: /node_modules/,
+	}),
+	typescript(),
 ]
 
 const productionPlugins = [...plugins, terser()]
 
 module.exports = ({
-  namespace,
-  cjs = true,
-  esm = true,
-  iife = !!pkg.browser,
-  sourcemap = true
+	namespace,
+	cjs = true,
+	esm = true,
+	iife = !!pkg.browser,
+	sourcemap = true,
 } = {}) => {
-  const name = "rematch." + namespace || pkgNamespace
+	const name = 'rematch.' + namespace || pkgNamespace
 
-  const rollupBuild = {
-    input,
-    plugins: env === "production" ? productionPlugins : plugins,
-    external: pkgDependencies,
-    output: []
-  }
+	const rollupBuild = {
+		input,
+		plugins: env === 'production' ? productionPlugins : plugins,
+		external: pkgDependencies,
+		output: [],
+	}
 
-  if (iife) {
-    rollupBuild.output.push({
-      name,
-      sourcemap,
-      file: pkg.browser,
-      format: "iife",
-      exports: "named"
-    })
-  }
+	if (iife) {
+		rollupBuild.output.push({
+			name,
+			sourcemap,
+			file: pkg.browser,
+			format: 'iife',
+			exports: 'named',
+		})
+	}
 
-  if (esm) {
-    rollupBuild.output.push({
-      sourcemap,
-      file: pkg.module,
-      format: "es",
-      exports: "named"
-    })
-  }
+	if (esm) {
+		rollupBuild.output.push({
+			sourcemap,
+			file: pkg.module,
+			format: 'es',
+			exports: 'named',
+		})
+	}
 
-  if (cjs) {
-    rollupBuild.output.push({
-      sourcemap,
-      file: pkg.main,
-      format: "cjs",
-      exports: "named"
-    })
-  }
+	if (cjs) {
+		rollupBuild.output.push({
+			sourcemap,
+			file: pkg.main,
+			format: 'cjs',
+			exports: 'named',
+		})
+	}
 
-  return rollupBuild
+	return rollupBuild
 }
