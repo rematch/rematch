ce0d6012876c02d26b9b4365003c2fc2f8a2682f
JiangWeixian <saber_wei@163.com>
2019-07-12 18:20:57 +0800

    chore: update redux@4.0.4

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -109,6 +109,7 @@ export interface RematchStore<M extends Models = Models, A extends Action = Acti
   getState(): RematchRootState<M>,
   model(model: Model): void,
   subscribe(listener: () => void): Redux.Unsubscribe,
+  [Symbol.observable](): Redux.Observable<RematchRootState<M>>
 }
 
 export type Action<P = any, M = any> = {
