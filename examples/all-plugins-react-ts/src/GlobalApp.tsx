282709d2b73ed4d580363a3883e09af86234a42f
Sergio Moreno <sergiomorenoalbert@gmail.com>
2021-01-29 19:42:27 +0100

    chore: testing on typescript app for integration suite

---
+++ examples/all-plugins-react-ts/src/GlobalApp.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import App from './AppWithHooks'
+import AppWithoutHooks from './AppWithoutHooks'
+import "./index.css";
+import "./switch.css";
+
+import { Provider } from 'react-redux'
+import { store } from './store'
+
+const PreviewSelector = () => {
+	const [showHooks, setShowHooks] = useState<boolean>(true);
+
+	return (
+		<div>
+			<button type="button" onClick={() => setShowHooks(true)}>
+				Show Application with hooks
+			</button>
+			<button type="button" onClick={() => setShowHooks(false)}>
+				Show Application with classes
+			</button>
+			{showHooks ? <App /> : <AppWithoutHooks />}
+		</div>
+	)
+}
+
+export const GlobalApp = () => (
+	<Provider store={store}>
+		<PreviewSelector />
+	</Provider>
+)
