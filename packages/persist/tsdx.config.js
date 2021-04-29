9e6c85d3a5c924502b9e47e4ebe5d6cfe0a8155f
Sergio Moreno <sergiomorenoalbert@gmail.com>
2021-04-29 22:14:01 +0200

    chore: closure compiler added

--- packages/persist/tsdx.config.js
+++ packages/persist/tsdx.config.js
@@ -2,7 +2,6 @@ const compiler = require('@ampproject/rollup-plugin-closure-compiler')
 
 module.exports = {
 	rollup(config) {
-		config.output.globals.Redux = 'Redux'
 		config.plugins.push(compiler())
 		return config
 	},
