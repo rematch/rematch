f128427c4fae8da729898262f6734db0eb5f9222
Sergio Moreno <sergiomorenoalbert@gmail.com>
2021-07-07 18:06:47 +0200

    chore: fix ci about prettier on examples

--- examples/updated-react/src/index.js
+++ examples/updated-react/src/index.js
@@ -3,21 +3,20 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { init } from '@rematch/core'
 import createUpdatedPlugin from '@rematch/updated'
+
 import * as models from './models'
 import App from './App'
 
-// create plugin
-const updated = createUpdatedPlugin()
-
 const store = init({
 	models,
-	plugins: [updated], // add to plugin list
+	// create plugin
+	plugins: [createUpdatedPlugin()], // add to plugin list
 })
 
 ReactDOM.render(
 	<React.StrictMode>
 		<Provider store={store}>
-				<App />
+			<App />
 		</Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
