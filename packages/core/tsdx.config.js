b63f7c108a30f522374da5b8c40d6418d11c0524
Sergio Moreno <22656541+semoal@users.noreply.github.com>
2021-08-13 10:50:30 +0200

    chore(build): reverted .mjs since .js with module works fine

--- packages/core/tsdx.config.js
+++ packages/core/tsdx.config.js
@@ -1,9 +1,5 @@
 module.exports = {
 	rollup(config) {
-		if (config.output.format === 'esm') {
-			config.output.file = config.output.file.replace('.js', '.mjs')
-		}
-
 		config.output.globals.Redux = 'Redux'
 		return config
 	},
--- packages/updated/tsdx.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	rollup(config) {
-		if (config.output.format === 'esm') {
-			config.output.file = config.output.file.replace('.js', '.mjs')
-		}
-
-		return config
-	},
-}
