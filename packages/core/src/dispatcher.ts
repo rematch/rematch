a9f3a64dbdab236fbb11ba6c394d42c43ddbddf6
Sushant <sushantdhiman@outlook.com>
2021-11-09 11:49:13 +0530

    fix: circular reference destruction works with models

--- packages/core/src/dispatcher.ts
+++ packages/core/src/dispatcher.ts
@@ -47,19 +47,17 @@ const createActionDispatcher = <
 
 /**
  * Creates a dispatcher object for a model - it contains a mapping from all
- * reducers and effects *names* to functions which dispatch their corresponding
- * actions.
+ * reducers to functions which dispatch their corresponding actions.
  */
-const createDispatcher = <
+export const createDispatcher = <
 	TModels extends Models<TModels>,
 	TExtraModels extends Models<TModels>,
 	TModel extends NamedModel<TModels>
 >(
 	rematch: RematchStore<TModels, TExtraModels>,
-	bag: RematchBag<TModels, TExtraModels>,
 	model: TModel
 ): void => {
-	const modelDispatcher = rematch.dispatch[model.name]
+	const modelDispatcher = rematch.dispatch[model.name] || {}
 
 	// map reducer names to dispatch actions
 	const modelReducersKeys = Object.keys(model.reducers)
@@ -73,7 +71,22 @@ const createDispatcher = <
 			false
 		)
 	})
+}
 
+/**
+ * Creates effects dispatcher for a model - it contains a mapping from all
+ * effects *names* to functions which dispatch their corresponding actions.
+ */
+export const createEffectDispatcher = <
+	TModels extends Models<TModels>,
+	TExtraModels extends Models<TModels>,
+	TModel extends NamedModel<TModels>
+>(
+	rematch: RematchStore<TModels, TExtraModels>,
+	bag: RematchBag<TModels, TExtraModels>,
+	model: TModel
+): void => {
+	const modelDispatcher = rematch.dispatch[model.name]
 	let effects: ModelEffects<TModels> = {}
 
 	// 'effects' might be actually a function creating effects
@@ -100,5 +113,3 @@ const createDispatcher = <
 		)
 	})
 }
-
-export default createDispatcher
